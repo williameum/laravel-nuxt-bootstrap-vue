eeae2f9effb6864c5fead71a922aadad3709cca9
William <william@trezlabs.com>
2020-01-22 11:33:29 -0600

    Boostrap-vue #1

---
+++ client/components/JobCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <b-col md="3">
+      <b-card
+        title="This is a Card"
+        img-src="https://picsum.photos/600/300/?image=25"
+        img-alt="Job-Info"
+        img-top
+        tag="article"
+        style="max-width:20rem"
+        class="mb-2"
+      >
+        <b-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam quo ipsa magnam debitis sapiente mollitia</b-card-text>
+        <b-button variant="primary">Apply</b-button>
+      </b-card>
+    </b-col>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
---
+++ client/pages/home2.vue
@@ -0,0 +1,81 @@
+<template>
+  <b-container class="bv-example-row">
+    <b-row align-v="center">
+      <b-col md="3">
+        <b-card
+          title="This is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width:20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam quo ipsa magnam debitis sapiente mollitia
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col >
+      <b-col md="3">
+        <b-card
+          title="This is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width:20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam quo ipsa magnam debitis sapiente mollitia
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+      <b-col md="3">
+        <b-card
+          title="This is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width:20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam quo ipsa magnam debitis sapiente mollitia
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+      <b-col md="3">
+        <b-card
+          title="This is a Card"
+          img-src="https://picsum.photos/600/300/?image=25"
+          img-alt="Job-Info"
+          img-top
+          tag="article"
+          style="max-width:20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsam quo ipsa magnam debitis sapiente mollitia
+          </b-card-text>
+          <b-button variant="primary">Apply</b-button>
+        </b-card>
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+import JobCard from "./../components/JobCard";
+export default {
+  name: "home",
+  components: {JobCard}
+};
+</script>
+
+<style>
+</style>
